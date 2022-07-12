8ee181829ddceee09632656a4fdd572497df0a0b
Aymeric Dominique <aymeric.dominique@gmail.com>
2022-07-12 18:26:10 +0200

    fix

--- src/components/TextItem.vue
+++ src/components/TextItem.vue
@@ -5,7 +5,7 @@
       C'est un fait, cela permet de mettre de l'argent dans un actif qui ne dévalue pas. Au contraire, dans beaucoup d'endroit, les prix augmentent.
     </p>
     <p>
-      De plus, certains mette en valeur le "rendement locatif". Un pourcentage qui définit le gain par mois engendre (loyer - dépense (mensualité)).
+      De plus, certains mette en valeur le "rendement locatif". Un pourcentage qui définit le gain par mois engendré.
       Comme ci le bénéfice à être possédant n'en était pas déjà un. Comme ci, le fait de faire rembourser son emprunt par un plus pauvre que soit était une chose raisonnable.
     </p>
   </div>
